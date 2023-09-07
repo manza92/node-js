59838c142ad8a25125f036fd44b193cc653b5c14
Darwin Mazano <manza_92@yahoo.com>
2023-09-06 19:35:03 -0600

    agregando chalk

--- calculadora.mjs
+++ calculadora.mjs
@@ -1,7 +1,17 @@
+import chalk from "chalk"
 import { suma, rest, mult, div } from "./operaciones.mjs";
 
-
+/*console.log(chalk.blue(suma(12, 64)))
+console.log(chalk.red(HOla))
 console.log(suma(12,64));
 console.log(rest(12,64));
 console.log(mult(12,64));
-console.log(div(12,64));
+console.log(div(12,64));*/
+const resultado = rest(1, 4)
+
+if (resultado > 5) {
+    console.log(chalk.red("resultado de la resta: " + resultado))
+    
+} else {
+    console.log(chalk.green("resultado de la resta: " + resultado))
+}
